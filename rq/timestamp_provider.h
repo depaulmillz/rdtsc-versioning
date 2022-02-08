1961ec6b897aee5f1dac760f6902c30fc2f17e9c
Olivia Grimes <oliviagrimes@dyn194055.cc.lehigh.edu>
2022-02-08 14:11:20 -0500

    combined bundling and vcas timestamps into one, fixed RDTSCP fences

--- rq/timestamp_provider.h
+++ rq/timestamp_provider.h
@@ -1,13 +1,13 @@
 // Olivia Grimes
 //
 // Timestamp provider
-// Timestamp interface for Bundling, Vcas, RDTSC and RDTSCP
+// Timestamp interface for "traditional" timestamping (Bundling, Vcas), RDTSC and RDTSCP
 // implementations of various data structures
 
 #pragma once
 
 #include <atomic>
-#define BUNDLE_MIN_TIMESTAMP 1LL
+#define MIN_TIMESTAMP 1LL
 
 typedef long long timestamp_t;
 
@@ -16,8 +16,8 @@ static thread_local int backoff_amt = 0;
 
 class TimestampProvider {
     public:
-        virtual timestamp_t Read() = 0;
-        virtual timestamp_t Advance() = 0;
+        virtual inline timestamp_t Read() = 0;
+        virtual inline timestamp_t Advance() = 0;
 };
 
 class RdtscTimestamp: public TimestampProvider {
@@ -34,11 +34,11 @@ class RdtscTimestamp: public TimestampProvider {
         }
     
     public:
-        timestamp_t Read() {
+        inline timestamp_t Read() {
             return readRdtsc();
         }
 
-        timestamp_t Advance() {
+        inline timestamp_t Advance() {
             return readRdtsc();
         }
 };
@@ -46,33 +46,30 @@ class RdtscTimestamp: public TimestampProvider {
 class RdtscpTimestamp: public TimestampProvider {
     private:
         inline timestamp_t readRdtscp() {
-            // TODO: fix memory issues -- LFENCE and MFENCE - is it this simple ?
             unsigned long long cycles_low, cycles_high;
             asm volatile (
                 "LFENCE\n\t"
                 "RDTSCP\n\t"
                 "mov %%rdx, %0\n\t"
-                "mov %%rax, %1\n\t"
-                "MFENCE\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+                "mov %%rax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
                 "%rax", "%rcx", "%rdx"
                 );
             return (((uint64_t)cycles_high << 32) | cycles_low);
         }
     
     public:
-        timestamp_t Read() {
+        inline timestamp_t Read() {
             return readRdtscp();
         }
 
-        timestamp_t Advance() {
+        inline timestamp_t Advance() {
             return readRdtscp();
         }
 };
 
-class BundlingTimestamp: public TimestampProvider {
+class BasicTimestamp: public TimestampProvider {
     private:
-        // Timestamp used by range queries to linearize accesses for bundling
-        std::atomic<timestamp_t> curr_timestamp_;
+        volatile timestamp_t curr_timestamp;
 
         inline void backoff(int amount) {
             if (amount == 0) return;
@@ -82,56 +79,14 @@ class BundlingTimestamp: public TimestampProvider {
         }
 
         inline timestamp_t getNextTS() {
-            timestamp_t ts = curr_timestamp_.load(std::memory_order_seq_cst);
+            timestamp_t ts = curr_timestamp;
             backoff(backoff_amt);
-            if (ts == curr_timestamp_.load(std::memory_order_seq_cst)) {
-                if (curr_timestamp_.fetch_add(1, std::memory_order_release) == ts)
+            std::atomic_thread_fence(std::memory_order_seq_cst);
+            if (ts == curr_timestamp) {
+                if (__sync_fetch_and_add(&curr_timestamp, 1) == ts)
                     backoff_amt /= 2;
                 else
                     backoff_amt *= 2;
-            }
-            if (backoff_amt < 1) backoff_amt = 1;
-            if (backoff_amt > 512) backoff_amt = 512;
-            return ts + 1;
-        }
-
-    public:
-        BundlingTimestamp() {
-            curr_timestamp_ = BUNDLE_MIN_TIMESTAMP;
-        }
-
-        timestamp_t Read() {
-            return curr_timestamp_.load();
-        }
-
-        timestamp_t Advance() {
-            return getNextTS();
-        }
-};
-
-class VcasTimestamp: public TimestampProvider {
-    private:
-        // TODO: difference with volatile vs atomic timestamp? (vcas vs bundling)
-        // --> can bundling and vcas be combined into one more general timestamp class, or nah
-        volatile timestamp_t timestamp;
-
-        inline void backoff(int amount) {
-            if (amount == 0) return;
-            volatile long long sum = 0;
-            int limit = amount;
-            for (int i = 0; i < limit; i++) sum += i;
-        }
-
-        inline timestamp_t takeSnapshot() {
-            timestamp_t ts = timestamp;
-            backoff(backoff_amt);
-            std::atomic_thread_fence(std::memory_order_seq_cst);
-            if (ts == timestamp) {
-            if (__sync_fetch_and_add(&timestamp, 1) == ts)
-                backoff_amt /= 2;
-            else
-                backoff_amt *= 2;
-            } else {
             }
             if (backoff_amt < 1) backoff_amt = 1;
             if (backoff_amt > 512) backoff_amt = 512;
@@ -139,15 +94,15 @@ class VcasTimestamp: public TimestampProvider {
         }
 
     public:
-        VcasTimestamp() {
-            timestamp = 1;
+        BasicTimestamp() {
+            curr_timestamp = MIN_TIMESTAMP;
         }
 
-        timestamp_t Read() {
-            return timestamp;
+        inline timestamp_t Read() {
+            return curr_timestamp;
         }
 
-        timestamp_t Advance() {
-            return takeSnapshot();
+        inline timestamp_t Advance() {
+            return getNextTS();
         }
 };
